4e0443ae9283db997acc927251782b575cc62ca6
stevekenes1607 <stevekenes@yahoo.com>
2022-04-23 12:30:16 +0100

    Initial Commit

---
+++ Controllers/ProductTypesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyStore.Data;
+using MyStore.Models;
+
+namespace MyStore.Areas.Admin.Controllers
+{
+    
+    public class ProductTypesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductTypesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            //var data = _context.ProductTypes.ToList();
+            return View(_context.ProductTypes.ToList());
+        }
+
+        //GET Create Action Method
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST Create Action Method
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductTypes productTypes)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.ProductTypes.Add(productTypes);
+                await _context.SaveChangesAsync();
+                //TempData["save"] = "Product type saved";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productTypes);
+        }
+
+    }
+}
